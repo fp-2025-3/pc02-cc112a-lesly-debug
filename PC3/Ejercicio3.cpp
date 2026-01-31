ef06601d5f4327ed3a02bafb828ced4c5868a0e0
Lesly <bltzrlesly@gmail.com>
2026-01-30 23:12:35 -0500

    Ejercicio 3

--- PC3/Ejercicio3.cpp
+++ PC3/Ejercicio3.cpp
@@ -7,45 +7,45 @@ const int NCOLS=9;
 void generar(int** matriz){
     for(int i=0;i<NFILAS;i++){
         for(int j=0;j<NCOLS;j++){
-            *(*matriz+i*NCOLS+j)=rand()%9+1;
+            *(*(matriz+i)+j)=rand()%9+1;
         }
     }
 }
 void imprimir(int** matriz){
     for(int i=0;i<NFILAS;i++){
         for(int j=0;j<NCOLS;j++){
-            cout<<*(*matriz+i*NCOLS+j)<<" ";
+            cout<<*(*(matriz+i)+j)<<" ";
         }
         cout<<endl;
     }
 }
 void actualizarValores(int** matriz,int fila,int col){
-    int valor=*(*matriz+fila*NCOLS+col);
+    int valor=*(*(matriz+fila)+col);
     for(int i=0;i<NFILAS;i++){
         int pos;
         for(pos=0;pos<NCOLS;pos++){
-            if(*(*matriz+i*NCOLS+pos)==valor){
+            if(*(*(matriz+i)+pos)==valor){
                 break;
             }
         }
         int desp=(col-pos+NCOLS)%NCOLS;
         int aux[NCOLS];
         for(int j=0;j<NCOLS;j++){
-            aux[(j+desp)%NCOLS]=*(*matriz+i*NCOLS+j);
+            aux[(j+desp)%NCOLS]=*(*(matriz+i)+j);
         }
         for(int j=0;j<NCOLS;j++){
-            *(*matriz+i*NCOLS+j)=aux[j];
+            *(*(matriz+i)+j)=aux[j];
         }
     }
     cout<<"Valor seleccionado: "<<valor<<endl;
 }
 int main(){
     srand(time(NULL));
-    int M[NFILAS][NCOLS];
-    int* p=&M[0][0];
-    int** A=&p;
-    int fila;
-    int columna;
+    int** A=new int*[NFILAS];
+    for(int i=0;i<NFILAS;i++){
+        *(A+i)=new int[NCOLS];
+    }
+    int fila,columna;
     generar(A);
     imprimir(A);
     cout<<"Fila inicial (1-9): ";
@@ -55,6 +55,9 @@ int main(){
     cout<<"Tablero final: "<<endl;
     actualizarValores(A,fila-1,columna-1);
     imprimir(A);
-    cout<<endl;
+    for(int i=0;i<NFILAS;i++){
+        delete[] A[i];//libera memoria de columnas
+    }
+    delete[] A;//libera memoria de filas
     return 0;
 }
