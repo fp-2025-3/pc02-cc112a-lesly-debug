33149d4327dec946275728d347e8bc88f348b254
Lesly <bltzrlesly@gmail.com>
2026-01-30 22:35:37 -0500

    Ejercicio 1

--- PC3/Ejercicio1.cpp
+++ PC3/Ejercicio1.cpp
@@ -4,17 +4,28 @@ int separar(char* texto,char* valores[]){
     int i=0;
     char* p=texto;
     while(*p!='\0'){
-        if((p==texto&&*p!=' ')||((*p!=' ')&&(*(p-1)==','))){
+        while(*p==' '){
+            p=p+1;
+        }
+        if(*p>='0'&&*p<='9'){
             valores[i]=p;
             i=i+1;
+            while(*p!=','&&*p!='\0'){//Permite avanzar hasta coma o fin
+                p=p+1;
+            }
+            if(*p==','){
+                *p='\0';
+                p=p+1;
+            }
+        }else{
+            p=p+1;
         }
-        p=p+1;
     }
     return i;
 }
 int convertir_entero(char* cadena){
     int n=0;
-    while(*cadena!='\0'){
+    while(*cadena>='0'&&*cadena<='9'){//Validando espacios (" 12")
         n=n*10+(*cadena-'0');
         cadena=cadena+1;
     }
@@ -23,10 +34,10 @@ int convertir_entero(char* cadena){
 int main(){
     char texto[]={"1, 2, 4, 7, 11"};
     char* valores[30];
-    int k=15;
+    int k=10;
+    cout<<texto<<endl;
     int n=separar(texto,valores);
     bool existen=false;
-    cout<<texto<<endl;
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             int p=convertir_entero(*(valores+i));
