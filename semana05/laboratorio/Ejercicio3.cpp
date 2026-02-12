c8f30f973cec06ce96134db97e0573ec3fed6b47
Lesly <bltzrlesly@gmail.com>
2026-02-11 20:56:47 -0500

    Ejercicio 3 corregido

--- semana05/laboratorio/Ejercicio3.cpp
+++ semana05/laboratorio/Ejercicio3.cpp
@@ -4,6 +4,7 @@
 #include<string>
 using namespace std;
 const int PARTIDOS=100;
+const int NUM_SELECCIONES=5;
 struct Seleccion{
     string nombre;
     int puntosTotales;
@@ -15,20 +16,19 @@ struct Seleccion{
 };
 void generandoValores(Seleccion s[],int n){
     for(int i=0;i<n;i++){
-        s[i].nombre="Pais_Seleccion"+to_string(i);
-        int pg=rand()%PARTIDOS;
-        s[i].PG=pg;
-        int pe=rand()%(PARTIDOS-pg);
-        s[i].PE=pe;
-        s[i].PP=PARTIDOS-pg-pe;
+        s[i].nombre="Pais_Seleccion "+to_string(i);
+        s[i].partidosJugados=PARTIDOS;
+        s[i].PG=rand()%(PARTIDOS+1);
+        s[i].PE=rand()%(PARTIDOS-s[i].PG+1);
+        s[i].PP=PARTIDOS-s[i].PG-s[i].PE;
         s[i].puntosTotales=s[i].PG*3+s[i].PE;
-        s[i].rendimiento=(float)s[i].puntosTotales/PARTIDOS;
+        s[i].rendimiento=(float)s[i].puntosTotales*100/(PARTIDOS*3);
     }
 }
 void mostrandoDatos(Seleccion s[],int n){
     cout<<"Seleccion\t\tPG\tPP\tPE\tPuntaje\t\tRendimiento\n";
     for(int i=0;i<n;i++){
-        cout<<s[i].nombre<<"\t\t"<<s[i].PG<<"\t"<<s[i].PP<<"\t"<<s[i].PE<<"\t"<<s[i].puntosTotales<<"\t\t"<<s[i].rendimiento<<endl;
+        cout<<s[i].nombre<<"\t"<<s[i].PG<<"\t"<<s[i].PP<<"\t"<<s[i].PE<<"\t"<<s[i].puntosTotales<<"\t\t"<<s[i].rendimiento<<endl;
     }
 }
 int indMejorRendimiento(Seleccion s[],int n){
@@ -36,16 +36,16 @@ int indMejorRendimiento(Seleccion s[],int n){
     for(int i=0;i<n;i++){
         if(s[i].rendimiento>s[indMax].rendimiento){
             indMax=i;
-            return i;
         }
     }
-    return -1;
+    return indMax;
 }
 int main(){
-    Seleccion selecciones[5];
-    generandoValores(selecciones,5);
-    mostrandoDatos(selecciones,5);
-    int pos=indMejorRendimiento(selecciones,5);
+    srand(time(nullptr));
+    Seleccion selecciones[NUM_SELECCIONES];
+    generandoValores(selecciones,NUM_SELECCIONES);
+    mostrandoDatos(selecciones,NUM_SELECCIONES);
+    int pos=indMejorRendimiento(selecciones,NUM_SELECCIONES);
     cout<<"La seleccion con mejor rendimiento es: Seleccion"<<pos<<" Rendimiento: "<<selecciones[pos].rendimiento;
     return 0;
 }
