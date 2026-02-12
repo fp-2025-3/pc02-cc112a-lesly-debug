be3d046af78aac1cc9794e843b4191a984a1a73e
Lesly <bltzrlesly@gmail.com>
2026-02-12 17:51:21 -0500

    Ejercicio 1 corregido

--- semana05/laboratorio/Ejercicio1.cpp
+++ semana05/laboratorio/Ejercicio1.cpp
@@ -6,12 +6,12 @@ struct Estudiante{
 };
 void leerDatos(Estudiante* e,int n){
     for(int i=0;i<n;i++){
-        cin.ignore();
         cout<<"Ingrese el nombre del estudiante "<<i+1<<" :";
         e[i].nombre=new char[20];
         cin.getline(e[i].nombre,20);
         cout<<"Ingrese la nota del estudiante "<<i+1<<" :";
         cin>>e[i].nota;
+        cin.ignore();
     }
 }
 int indMayorNota(Estudiante* e,int n){
@@ -19,19 +19,22 @@ int indMayorNota(Estudiante* e,int n){
     for(int i=0;i<n;i++){
         if(e[i].nota>e[indMax].nota){
             indMax=i;
-            return i;
         }
     }
-    return -1;
+    return indMax;
 }
 int main(){
     int n;
     cout<<"Ingrese el numero de estudiantes: ";
     cin>>n;
+    cin.ignore();
     Estudiante* estudiantes=new Estudiante[n];
     leerDatos(estudiantes,n);
     int pos=indMayorNota(estudiantes,n);
     cout<<"El estudiante con la mayor nota es: "<<estudiantes[pos].nombre<<" Nota: "<<estudiantes[pos].nota;
+    for(int i=0;i<n;i++){
+        delete[] estudiantes[i].nombre;
+    }
     delete[] estudiantes;
     return 0;
 }
