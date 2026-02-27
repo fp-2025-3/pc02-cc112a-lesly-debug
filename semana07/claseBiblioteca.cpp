24429c70acffbf4b1a7815e15fc44cd502f4013c
Lesly <bltzrlesly@gmail.com>
2026-02-26 19:50:28 -0500

    Implementando herencia de clase

--- semana07/claseBiblioteca.cpp
+++ semana07/claseBiblioteca.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class Biblioteca{//Implementar herencia a esta clase
+class Biblioteca{
     private:
     int codigo;
     string titulo;
     bool prestado;
     public:
-    Biblioteca(int c=0,const char* t=""){
+    Biblioteca(int c=0,const string& t=""){
         if(c<0){
             cout<<"Codigo invalido\n";
+            c=0;
         }
         codigo=c;
-        strcpy(titulo,t);
+        titulo=t;
         prestado=false;
     }
     bool prestar(){
@@ -32,8 +33,22 @@ class Biblioteca{//Implementar herencia a esta clase
         }
     }
 };
+class LibroDigital:public Biblioteca{
+    private:
+    double tamanioMB;
+    public:
+    LibroDigital(int c,const string& t,double tam):Biblioteca(c,t){
+        tamanioMB=tam;
+    }
+    void mostrarDigital(){
+        mostrar();
+        cout<<"Tamanio: "<<tamanioMB<<" MB\n";
+    }
+};
 int main(){
     Biblioteca lib(1,"El Principito");
     lib.mostrar();
+    LibroDigital ebook(2,"Los viajes de Gulliver",5.4);
+    ebook.mostrarDigital();
     return 0;
 }
