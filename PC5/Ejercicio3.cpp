f9a45926e98002f95693937b0265aaa8f881842d
Lesly <bltzrlesly@gmail.com>
2026-03-01 00:11:49 -0500

    Subiendo ejercicio 3

--- PC5/Ejercicio3.cpp
+++ PC5/Ejercicio3.cpp
@@ -5,15 +5,43 @@ using namespace std;
 int main(){
     ofstream archivo;
     archivo.open("grafico.txt");
-    for(int i=0;i<22;i++){
-        for(int j=0;j<81;j++){
-            if(j==0){
-                archivo<<"|"<<endl;
-            }
-            if(i==10){
-                archivo<<"-";
-            }
+    if(!archivo){
+        cerr<<"Error al abrir el archivo.\n";
+        return -1;
+    }
+    const int FILAS=21;
+    const int COLUMNAS=80;
+    double x;
+    double y;
+    double paso=(2*M_PI)/(COLUMNAS-1);
+    char grafico[FILAS][COLUMNAS];
+    for(int i=0;i<FILAS;i++){
+        for(int j=0;j<COLUMNAS;j++){
+            grafico[i][j]=' ';
         }
     }
+    int filaEjeX=FILAS/2;
+    for(int j=0;j<COLUMNAS;j++){
+        grafico[filaEjeX][j]='-';
+    }
+    for(int i=0;i<FILAS;i++){
+        grafico[i][0]='|';
+    }
+    for(int j=0;j<COLUMNAS;j++){
+        x=j*paso;
+        y=sin(5*x);
+        int fila=(int)((1 - y)*(FILAS-1)/2);
+        if(fila>=0&&fila<FILAS){
+            grafico[fila][j]='*';
+        }
+    }
+    for(int i=0;i<FILAS;i++){
+        for(int j=0; j<COLUMNAS;j++){
+            archivo<<grafico[i][j];
+        }
+        archivo<< endl;
+    }
+    archivo.close();
+    cout<<"Archivo grafico.txt generado correctamente.\n";
     return 0;
 }
