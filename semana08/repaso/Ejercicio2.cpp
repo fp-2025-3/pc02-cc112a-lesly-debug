08d0436bfb61d421700d40a175ff626278f53159
Lesly <bltzrlesly@gmail.com>
2026-03-05 00:10:17 -0500

    Ejercicio de cuenta bancaria

--- semana08/repaso/Ejercicio2.cpp
+++ semana08/repaso/Ejercicio2.cpp
@@ -7,20 +7,19 @@ struct CuentaBancaria{
     double saldo;
     bool activa;
 };
-void crearArchivo(const char* nombreArchivo,int n){
-    fstream archivo;
-    archivo.open(nombreArchivo,ios::binary|ios::in|ios::out);
+void crearArchivo(int N){
+    fstream archivo("cuentas.dat",ios::binary|ios::in|ios::out);
     if(!archivo){
-        archivo.open(nombreArchivo,ios::out|ios::binary);
+        archivo.open("cuentas.dat",ios::out|ios::binary);
         archivo.close();
-        archivo.open(nombreArchivo,ios::binary|ios::in|ios::out);
+        archivo.open("cuentas.dat",ios::binary|ios::in|ios::out);
     }
     CuentaBancaria cuenta;
-    for(int i=0;i<n;i++){
+    for(int i=0;i<N;i++){
         cout<<"Numero de cuenta "<<i+1<<endl;
-        cuenta.numeroCuenta=i+1;
-        int posicion=sizeof(CuentaBancaria)*(cuenta.numeroCuenta-1);
-        archivo.seekg(posicion);
+        cin>>cuenta.numeroCuenta;
+        int posicion=(cuenta.numeroCuenta-1)*sizeof(CuentaBancaria);
+        archivo.seekg(posicion,ios::beg);
         CuentaBancaria temp;
         archivo.read((char*)&temp,sizeof(CuentaBancaria));
         if(archivo){
@@ -28,6 +27,7 @@ void crearArchivo(const char* nombreArchivo,int n){
             i=i-1;
         }else{
             archivo.clear();
+            archivo.seekp(posicion,ios::beg);
             cout<<"Titular: ";
             cin.ignore();
             cin.getline(cuenta.titular,40);
@@ -37,24 +37,139 @@ void crearArchivo(const char* nombreArchivo,int n){
             archivo.write((char*)&cuenta,sizeof(CuentaBancaria));
         }
     }
-    for(int i=0;i<n;i++){
-        archivo.read((char*)&cuenta,sizeof(CuentaBancaria));
-        for(int j=0;j<n;j++){
-            archivo.read((char*)&cuenta,sizeof(CuentaBancaria));
-            CuentaBancaria temp=cuenta;
-            if(cuenta.numeroCuenta>temp.numeroCuenta){
-                archivo.write((char*)&temp,sizeof(CuentaBancaria));
-            }else{
-                archivo.write((char*)&cuenta,sizeof(CuentaBancaria));
-            }
+    archivo.close();
+}
+bool buscarCuenta(int numero,CuentaBancaria& cuenta){
+    ifstream archivo("cuentas.dat",ios::binary);
+    if(!archivo){
+        cerr<<"Error al abrir el archivo.\n";
+        return false;
+    }
+    int posicion=(numero-1)*sizeof(CuentaBancaria);
+    archivo.seekg(posicion,ios::beg);
+    archivo.read((char*)&cuenta,sizeof(CuentaBancaria));
+    if(!archivo){
+        archivo.close();
+        return false;
+    }
+    if(cuenta.numeroCuenta==numero){
+        archivo.close();
+        return true;
+    }
+    archivo.close();
+    return false;
+}
+void depositarCuenta(int numero,CuentaBancaria& cuenta,double monto){
+    if(monto<=0){
+        cout<<"[Error] El monto debe ser mayor que cero.\n";
+        return;
+    }
+    fstream archivo("cuentas.dat",ios::binary|ios::in|ios::out);
+    if(!archivo){
+        cout<<"Error al abrir el archivo.\n";
+        return;
+    }
+    if(buscarCuenta(numero,cuenta)){
+        if(!cuenta.activa){
+            cout<<"Cuenta inactiva.\n";
+            archivo.close();
+            return;
+        }
+        int posicion=(numero-1)*sizeof(CuentaBancaria);
+        cuenta.saldo=cuenta.saldo+monto;
+        archivo.seekp(posicion,ios::beg);
+        archivo.write((char*)&cuenta,sizeof(CuentaBancaria));
+        cout<<"[Deposito] Se deposito S/."<<monto<<".Saldo actual: "<<cuenta.saldo<<endl;
+        archivo.close();
+    }else{
+        cout<<"Numero de cuenta incorrecto.\n";
+        archivo.close();
+        return;
+    }
+}
+void retirarCuenta(int numero,CuentaBancaria& cuenta,double monto){
+    if(monto<=0){
+        cout<<"[Error] El monto debe ser mayor que cero.\n";
+    }
+    fstream archivo("cuentas.dat",ios::binary|ios::in|ios::out);
+    if(!archivo){
+        cout<<"Error al abrir el archivo.\n";
+        return;
+    }
+    if(buscarCuenta(numero,cuenta)){
+        if(!cuenta.activa){
+            cout<<"Cuenta inactiva.\n";
+            archivo.close();
+            return;
+        }
+        if(cuenta.saldo<monto){
+            cout<<"Fondos insuficientes.\n";
+            archivo.close();
+            return;
+        }else{
+            int posicion=(numero-1)*sizeof(CuentaBancaria);
+            cuenta.saldo=cuenta.saldo-monto;
+            archivo.seekp(posicion,ios::beg);
+            archivo.write((char*)&cuenta,sizeof(CuentaBancaria));
+            cout<<"[Retiro] Se retiro S/."<<monto<<". Saldo actual: "<<cuenta.saldo<<endl;
+            archivo.close();
+        }
+    }else{
+        cout<<"Numero de cuenta incorrecto.\n";
+        archivo.close();
+        return;
+    }
+}
+void desactivarCuenta(int numero,CuentaBancaria& cuenta){
+    fstream archivo("cuentas.dat",ios::binary|ios::in|ios::out);
+    if(!archivo){
+        cout<<"Error al abrir el archivo.\n";
+        return;
+    }
+    if(buscarCuenta(numero,cuenta)){
+        int posicion=(numero-1)*sizeof(CuentaBancaria);
+        if(!cuenta.activa){
+            cout<<"La cuenta ya esta inactiva.\n";
+            archivo.close();
+            return;
+        }
+        cuenta.activa=false;
+        archivo.seekp(posicion,ios::beg);
+        archivo.write((char*)&cuenta,sizeof(CuentaBancaria));
+        cout<<"Cuenta desactivada correctamente.\n";
+        archivo.close();
+    }else{
+        cout<<"Numero de cuenta incorrecto.\n";
+        archivo.close();
+        return;
+    }
+}
+void mostrarCuentasActivas(){
+    ifstream archivo("cuentas.dat",ios::binary);
+    if(!archivo){
+        cout<<"Error al abrir el archivo.\n";
+        return;
+    }
+    CuentaBancaria cuenta;
+    cout<<"\nCUENTAS ACTIVAS\n";
+    while(archivo.read((char*)&cuenta,sizeof(CuentaBancaria))){
+        if(cuenta.activa){
+            cout<<"Numero de cuenta: "<<cuenta.numeroCuenta<<endl;
+            cout<<"Titular: "<<cuenta.titular<<endl;
+            cout<<"Saldo: "<<cuenta.saldo<<endl;
+            cout<<"-----------------------\n";
         }
     }
     archivo.close();
 }
+
 int main(){
-    int n;
-    cout<<"Ingrese N=";
-    cin>>n;
-    crearArchivo("cuentas.dat",n);
+    int N=2;
+    crearArchivo(N);
+    CuentaBancaria cuenta;
+    depositarCuenta(1,cuenta,100);
+    retirarCuenta(1,cuenta,50);
+    desactivarCuenta(1,cuenta);
+    mostrarCuentasActivas();
     return 0;
 }
